343961457311c888a25181a7f29547ce46cb7767
wiuaahq-pixel <wiuaahq@gmail.com>
2026-04-17 15:09:51 +0530

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,15 +1,11 @@
 #include <jni.h>
 #include <pthread.h>
 #include <unistd.h>
-#include <cstring>
-#include <string>
-
-#include "modmenu.h"
 #include "zygisk.hpp"
+#include "modmenu.h"
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
-using zygisk::ServerSpecializeArgs;
 
 class MyModule : public zygisk::ModuleBase {
 public:
@@ -19,26 +15,19 @@ public:
     }
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
-        // Force the option to prevent bootloops on Android 15
+        // Android 15 Safety: Tell Zygisk to clean up properly
         api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
-        
-        // Don't check package here; just enable and let hack_thread decide
-        enable_hack = 1; 
-        }
-
-
+    }
 
     void postAppSpecialize(const AppSpecializeArgs *args) override {
-        if (enable_hack) {
-            pthread_t ntid;
-            pthread_create(&ntid, NULL, hack_thread, NULL);
-        }
+        // Launch everything in a separate thread so Zygote can finish instantly
+        pthread_t ntid;
+        pthread_create(&ntid, NULL, hack_thread, NULL);
     }
 
 private:
     Api *api;
     AppSpecializeArgs *args;
-    int enable_hack = 0;
 };
 
 REGISTER_ZYGISK_MODULE(MyModule)
